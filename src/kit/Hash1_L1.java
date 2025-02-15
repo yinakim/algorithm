0e33da649b4a462d6638b4da99a4097ac68820c8
yina <yinaofficeg@gmail.com>
2025-02-15 22:04:48 +0900

    고득점kit - Hash, level1 1. Hash1_L1 : 폰켓몬 (key-value 이용하기) 2. Hash2_L1 : 완주하지 못한 선수 (key-value 이용하기)

---
+++ src/kit/Hash1_L1.java
@@ -0,0 +1,58 @@
+package kit;
+import java.util.*;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+// 폰켓몬
+public class Hash1_L1 {
+    public static void main(String[] args) {
+        /**
+         * 입출력 예
+         * nums	            result
+         * [3,1,2,3]	    2
+         * [3,3,3,2,2,4]	3
+         * [3,3,3,2,2,2]	2
+         */
+
+        int[] arr1 = new int[]{3,1,2,3};
+        int[] arr2 = new int[]{3,3,3,2,2,4};
+        System.out.println(getNums(arr1));
+        System.out.println(getNums(arr2));
+
+    }
+
+    private static int getNums(int[] nums) {
+        /**
+         * 최대한 다양한 종류의 폰켓몬을 포함해서 N/2마리를 선택
+         *
+         *  N마리 폰켓몬의 종류 번호가 담긴 배열 nums ===> 중복없이 봤을 때 key 배열로 쓸수있음, Set? 이용?
+         *  N/2마리의 폰켓몬을 선택하는 방법 중, 가장 많은 종류의 폰켓몬을 선택하는 방법
+         *
+         *
+         *  (제한)
+         *  1. nums의 길이(N)는 1 이상 10,000 이하의 자연수이며, 항상 짝수 (1만)
+         *  2. 폰켓몬의 종류 번호는 1 이상 200,000 이하의 자연수로 나타냅니다. (20만)
+         *  3. 가장 많은 종류의 폰켓몬을 선택하는 방법이 여러 가지인 경우에도, 선택할 수 있는 폰켓몬 종류 개수의 최댓값 하나만 return 하면 됩니다.
+         */
+        int answer = 0;
+
+        // N값 구하기
+        int N = nums.length;
+
+        // Set 생성, 종류번호넣기
+        Set set = new HashSet<>();
+        for(int i=0; i < N; i++){
+            set.add(nums[i]);
+        }
+
+        int cnt = set.size();
+        int half = N/2;
+        if( cnt <= half) {
+            answer = cnt;
+        } else if (cnt > half) {
+            answer = half;
+        }
+        return answer;
+    }
+
+
+}
---
+++ src/kit/Hash2_L1.java
@@ -0,0 +1,86 @@
+package kit;
+
+import java.util.*;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42576
+public class Hash2_L1 {
+
+
+    public static void main(String[] args) {
+
+        /**
+         입출력 예
+         participant	completion	return
+         ["leo", "kiki", "eden"]	["eden", "kiki"]	"leo"
+         ["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]	"vinko"
+         ["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
+         */
+
+        String[] participant = new String[]{"leo", "kiki", "eden"};
+        String[] completion = new String[]{"eden", "kiki"};
+        System.out.println(sol(participant, completion));
+    }
+
+    private static String sol(String[] participant, String[] completion) {
+        /**
+         * 참여한 선수들의 이름이 담긴 배열 participant
+           완주한 선수들의 이름이 담긴 배열 completion
+
+           완주하지 못한 선수의 이름을 return
+         ------------------------------------
+         * 참여한 선수의 수는 1명 이상 100,000명(십만명) 이하입니다. ===> 10000 개 이상이면 이중for 쓰면안됨
+         * completion의 길이는 participant의 길이보다 1 작습니다.
+         * 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자
+         * 참가자 중에는 동명이인이 있을 수 있습니다.
+         */
+
+        String answer = "";
+
+        Map<String, Integer> map = new HashMap<>();
+
+        // 참가자 이름(key), 몇명(value) map에 저장
+        for (String p : participant){
+            map.put(p, map.getOrDefault(p, 0)+1); // 동명이인이 있는 경우 +1해야되니까
+        }
+
+        // 완주자 이름에 해당되는 수를 map 에서 제거
+        for(String c : completion){
+            map.put(c, map.get(c) - 1);
+        }
+
+        // 인원수(value) > 1 인 이름(key) 반환
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() > 0) return entry.getKey();
+        }
+
+        // 없는 경우 공백 리턴
+        return answer;
+    }
+
+    // 다시풀기
+    private static String sol2(String[] participant, String[] completion) {
+        String answer = "";
+
+        Map<String, Integer> map = new HashMap<>();
+        // 참가자 이름(key), 인원수(val) 를 map에 추가
+        for(String p : participant){
+            map.put(p, map.getOrDefault(p, 0) + 1);
+
+        }
+
+        // 완주한 이름을 감소 : val - 1
+        for(String c : completion){
+            map.put(c, map.get(c)-1);
+        }
+
+        // map 모든 요소를 꺼내서 val > 0인 key값 리턴, 없으면 공백리턴
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() > 0) {
+                answer = entry.getKey();
+            }
+        }
+
+        return answer;
+    }
+
+}
