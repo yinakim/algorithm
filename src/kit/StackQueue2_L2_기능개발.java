cd68be4d1057212b357c18860004e1fca7bd84dc
yina <yinaofficeg@gmail.com>
2025-02-17 00:20:42 +0900

    TODO 고득점kit - 스택,큐 level2 / StackQueue2_L2_기능개발

---
+++ src/kit/StackQueue2_L2_기능개발.java
@@ -0,0 +1,26 @@
+package kit;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42586
+public class StackQueue2_L2_기능개발 {
+
+    public static void main(String[] args) {
+        /**
+         * 입출력 예
+         * progresses	                speeds	            return
+         * [93, 30, 55]	                [1, 30, 5]	        [2, 1]
+         * [95, 90, 99, 99, 80, 99]	    [1, 1, 1, 1, 1, 1]	[1, 3, 2]
+         */
+
+        int[] progresses = new int[]{93, 30, 55};
+        int[] speeds = new int[]{1, 30, 5};
+        System.out.println(solution(progresses, speeds));
+    }
+
+    private static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+
+
+
+}
