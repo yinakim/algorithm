509238ff603a3b96262bd37b750d4aa35d4b3383
yina <yinaofficeg@gmail.com>
2025-03-08 08:46:36 +0900

    chap11. 자료구조 - Stack 이용, 올바른 괄호 [오답케이스, 정답코드 추가] 근데 Stack을 굳이 이용했지만 이용안하는게 더 간단하긴함;

--- src/chap11_DataStructure/ex/level2/Stack_올바른괄호.java
+++ src/chap11_DataStructure/ex/level2/Stack_올바른괄호.java
@@ -5,7 +5,32 @@ import java.util.*;
 // https://school.programmers.co.kr/learn/courses/30/lessons/12909
 // 고득점kit, Stack
 public class Stack_올바른괄호 {
-    private static boolean useSimpleSolution(String s) {
+    private static boolean useSimpleSolution_정답(String s) {
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                count++;
+            } else {
+                count--;
+            }
+            if (count < 0) return false; // ')'가 먼저 나온 경우
+        }
+
+        return count == 0;
+    }
+    private static boolean useSimpleSolution_오답(String s) {
+        /**
+         ")(()" 같은 경우에도 마지막 문자가 ')'이므로 true가 반환될 수 있어 논리가 잘못됨
+         문제에서 명시하지 않았지만 '올바른 괄호' 라고 하면 짝이 지어져야 한다고함.. ;;
+
+         "(())("
+
+         [오답케이스]
+
+         "((())"
+         "())(()"
+         */
         char[] charArray = s.toCharArray();
         // [1] 맨앞글자가 닫는 소괄호면 무조건 false
         if(charArray[0] == ')') return false;
@@ -44,10 +69,21 @@ public class Stack_올바른괄호 {
         System.out.println(useStack("(()("));
         System.out.println();
 
-        System.out.println(useSimpleSolution("()()"));
-        System.out.println(useSimpleSolution("(())()"));
-        System.out.println(useSimpleSolution(")()("));
-        System.out.println(useSimpleSolution("(()("));
+        System.out.println(useSimpleSolution_오답("()()"));
+        System.out.println(useSimpleSolution_오답("(())()"));
+        System.out.println(useSimpleSolution_오답(")()("));
+        System.out.println(useSimpleSolution_오답("(()("));
+        System.out.println("오답케이스, false나와야 함 : " + useSimpleSolution_오답("((())")); // true 나옴, 짝이 이루어지지 않은 ( 하나 남기 때문
+        System.out.println("오답케이스, false나와야 함 : " + useSimpleSolution_오답("())(()")); // true 나옴, ")(" 같은 경우를 포함하여 괄호가 올바르게 닫히지 않음
+        System.out.println();
+
+        System.out.println(useSimpleSolution_정답("()()"));
+        System.out.println(useSimpleSolution_정답("(())()"));
+        System.out.println(useSimpleSolution_정답(")()("));
+        System.out.println(useSimpleSolution_정답("(()("));
+        System.out.println("오답케이스, false나와야 함 : " + useSimpleSolution_정답("((())"));
+        System.out.println("오답케이스, false나와야 함 : " + useSimpleSolution_정답("())(()"));
+        System.out.println();
     }
 }
 /**
